e5338a4e09c0c4ffdbdac3b570a8378b9f16f5a0
lsy-it-1995 <garyshaoyu@gmail.com>
2022-04-12 18:30:51 +0800

    update

--- C/variables and data types/main.c
+++ C/variables and data types/main.c
@@ -3,5 +3,16 @@
 int main() {
     char c = 'a';
     short s = 12;
-    
+    int i = 342;
+    long l = 1231432;
+    float f = 123.54;
+    double d = 23424.534254;
+    printf("%f", f);
+    printf("size of char is %d\n", sizeof(char));
+    printf("size of short is %d\n", sizeof(short));
+    printf("size of int is %d\n", sizeof(int));
+    printf("size of float is %d\n", sizeof(float));
+    printf("size of long is %d\n", sizeof(long));
+    printf("size of double is %d\n", sizeof(double));
+    return 0;
 }
