16afab336906d8d4d4be49620415bd1c65e35b35
lsy-it-1995 <garyshaoyu@gmail.com>
2022-04-12 18:22:10 +0800

    update

--- C/variables and data types/main.c
+++ C/variables and data types/main.c
@@ -1,16 +1,7 @@
 #include <stdio.h>      
 
 int main() {
-    short a = 65536;
-    long b;
-    long long c;
-    long double d;
+    char c = 'a';
+    short s = 12;
     
-        printf("size of short = %d bytes\n", sizeof(a));
-        printf("size of long = %d bytes\n", sizeof(b));
-
-    
-    printf("size of long long = %d bytes\n", sizeof(c));
-    printf("size of long double= %d bytes\n", sizeof(d));
-    return 0;
 }
